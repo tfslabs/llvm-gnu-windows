d0b7fa821a701f1d23182d19d5c711ed5f7eb3b0
ねこ 白い <shiroineko.tfs@gmail.com>
2024-02-16 22:28:26 +0700

    Create build.bat

---
+++ build.bat
@@ -0,0 +1,14 @@
+@echo off
+cd /d "%~dp0"
+
+md .bin && cd .bin
+md bootstrap
+md makefiles && cd makefiles
+
+set /a BUILDCPU=%NUMBER_OF_PROCESSORS%/2
+
+::llvm
+md llvm && cd llvm
+cmake -G "Unix Makefiles" -DDEFAULT_SYSROOT="$(xcrun --show-sdk-path)" -DLLVM_ENABLE_PROJECTS="clang;libcxx;libcxxabi;lld;lldb" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=../../bootstrap ../../../llvm
+make -j%BUILDCPU%
+make install
