fc892abe3a78c0caae7e3ebe7cf346cbee80d73d
ねこ 白い <shiroineko.tfs@gmail.com>
2024-02-17 13:26:39 +0700

    Update build.bat

--- build.bat
+++ build.bat
@@ -9,6 +9,6 @@ set /a BUILDCPU=%NUMBER_OF_PROCESSORS%/2
 
 ::llvm
 md llvm && cd llvm
-cmake -G "Unix Makefiles" -DLLVM_ENABLE_PROJECTS="clang;lld;lldb" -DCMAKE_PREFIX_PATH=../../bootstrap ../../../llvm
+cmake -G "Unix Makefiles" -DLLVM_ENABLE_PROJECTS="clang;lld;lldb" -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_PREFIX_PATH=../../bootstrap ../../../llvm
 make -j4
 make install
