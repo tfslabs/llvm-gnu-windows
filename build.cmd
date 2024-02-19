e023e0205c87a6ae9361e26daab4021c63345910
ねこ 白い <shiroineko.tfs@gmail.com>
2024-02-19 20:11:36 +0700

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -9,6 +9,6 @@ set /a BUILDCPU=%NUMBER_OF_PROCESSORS%/2
 
 ::llvm
 md llvm && cd llvm
-cmake -G "Unix Makefiles" -DLLVM_ENABLE_PROJECTS="bolt;clang;clang-tools-extra;compiler-rt;cross-project-tests;libc;libclc;lld;lldb;mlir;openmp;polly;pstl;flang" -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_PREFIX_PATH=../../bootstrap ../../../llvm
+cmake -G "Unix Makefiles" -DLLVM_ENABLE_PROJECTS="clang;lld;lldb" -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_PREFIX_PATH=../../bootstrap ../../../llvm
 make -j4
 make install
