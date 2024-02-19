b0efa5b2feb75411655cf5bd39deeb0087e4f2f2
ねこ 白い <shiroineko.tfs@gmail.com>
2024-02-19 20:12:45 +0700

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -10,5 +10,5 @@ set /a BUILDCPU=%NUMBER_OF_PROCESSORS%/2
 ::llvm
 md llvm && cd llvm
 cmake -G "Unix Makefiles" -DLLVM_ENABLE_PROJECTS="clang;lld;lldb" -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_PREFIX_PATH=../../bootstrap ../../../llvm
-make -j4
-make install
+cmake --build .
+cmake --install .
