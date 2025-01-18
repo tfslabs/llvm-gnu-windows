23d7c34df0b51d1e339619baac7cea2fd34404b2
shiroinekotfs <shiroineko.tfs@gmail.com>
2025-01-18 13:46:14 +0700

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -5,6 +5,6 @@ md .bin
 md .cmake
 
 cd .cmake
-cmake -G "Unix Makefiles" -S ../llvm -B . -DLLVM_ENABLE_PROJECTS="clang;lld" -DCMAKE_INSTALL_PREFIX="../.bin" -DCMAKE_BUILD_TYPE=MinSizeRel -DLLVM_INSTALL_UTILS=ON
+cmake -G "Unix Makefiles" -S ../llvm -B . -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DCMAKE_INSTALL_PREFIX="../.bin" -DCMAKE_BUILD_TYPE=MinSizeRel -DLLVM_INSTALL_UTILS=ON
 cmake --build . --parallel
 cmake --install .
